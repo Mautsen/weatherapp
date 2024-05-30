ac63abe3849520be6466c5b5f1020797853819df
Matias Leppänen <matias.joosua@hotmail.com>
2024-05-30 13:06:18 +0300

    Add: F/C-button position changed and KDoc added for App.kt

--- LocationApp/app/src/main/java/com/example/myapplication/composables/App.kt
+++ LocationApp/app/src/main/java/com/example/myapplication/composables/App.kt
@@ -10,17 +10,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -39,16 +29,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.LocationOn
 import androidx.compose.material.icons.rounded.Search
-import androidx.compose.material.icons.rounded.Star
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.DockedSearchBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SearchBar
+import androidx.compose.material3.*
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -60,20 +41,22 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import androidx.wear.compose.material.Colors
 import com.example.myapplication.ui.theme.Typography
 import com.example.myapplication.ui.theme.antonRegular
 import com.example.myapplication.ui.theme.gradientBackground
 import com.example.myapplication.ui.theme.valeraRound
 
-
-
+/**
+ * The main composable function for the application. This function sets up the UI and handles
+ * the necessary permissions and state management.
+ */
 @SuppressLint("DefaultLocale")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App() {
     val viewModel: MainViewModel = viewModel()
 
+    // Launch a permission request for location access
     val permissionLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestMultiplePermissions(),
         onResult = { permissions ->
@@ -84,6 +67,7 @@ fun App() {
         }
     )
 
+    // Request location permissions when the composable is first launched
     LaunchedEffect(Unit) {
         permissionLauncher.launch(
             arrayOf(
@@ -105,6 +89,7 @@ fun App() {
     val weather = weatherState.value
     val dailyForecast = dailyForecastState.value
 
+    // Prepare data for display
     val latitude = location?.latitude ?: "Not available"
     val longitude = location?.longitude ?: "Not available"
     val temperature = weather?.current?.temperature ?: 0.0
@@ -112,9 +97,7 @@ fun App() {
         "${temperature}°C"
     } else {
         val fahrenheitTemp = temperature * 9.0 / 5.0 + 32.0
-        val formattedFahrenheit = String.format("%.1f°F", fahrenheitTemp)
-
-        formattedFahrenheit
+        String.format("%.1f°F", fahrenheitTemp)
     }
     val weatherCode = weather?.current?.weatherCode ?: -1
 
@@ -129,86 +112,87 @@ fun App() {
         else -> R.drawable.default_image
     }
 
+    // Update loading state based on location and weather data
     LaunchedEffect(location, weather) {
         isDataLoading = location == null || weather == null || dailyForecast == null
     }
 
     Column(modifier = Modifier.padding(16.dp)) {
-        Row(
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = 16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
+                .padding(bottom = 16.dp)
         ) {
-            SearchBar(
-                modifier = Modifier.weight(1f).padding(end = 8.dp),
-                query = text,
-                onQueryChange = { text = it },
-                onSearch = { active = false },
-                active = active,
-                onActiveChange = { active = it },
-                placeholder = { Text("Search location") },
-                leadingIcon = { Icon(Icons.Rounded.Search, contentDescription = null) },
-
-            ) {
-                LazyColumn(
+            Column {
+                SearchBar(
                     modifier = Modifier.fillMaxWidth(),
-                    contentPadding = PaddingValues(16.dp),
-                    verticalArrangement = Arrangement.spacedBy(4.dp)
+                    query = text,
+                    onQueryChange = { text = it },
+                    onSearch = { active = false },
+                    active = active,
+                    onActiveChange = { active = it },
+                    placeholder = { Text("Search location") },
+                    leadingIcon = { Icon(Icons.Rounded.Search, contentDescription = null) },
                 ) {
-                    items(listOf("Valkeakoski", "Kerava", "GPS")) { city ->
-                        ListItem(
-                            modifier = Modifier.clickable {
-                                text = city
-                                active = false
-                                when (city.lowercase()) {
-                                    "valkeakoski" -> viewModel.setCustomLocation(61.2628, 24.0316)
-                                    "kerava" -> viewModel.setCustomLocation(60.4039, 25.1015)
-                                    "gps" -> viewModel.useCurrentLocation()
-                                }
-                            },
-                            headlineContent = {
-                                Text(
-                                    text = city,
-                                    style = MaterialTheme.typography.titleMedium
-                                )
-                            },
-                            supportingContent = {
-                                Text("Select $city")
-                            },
-                            leadingContent = {
-                                Icon(Icons.Rounded.LocationOn, contentDescription = null)
-                            },
+                    LazyColumn(
+                        modifier = Modifier.fillMaxWidth(),
+                        contentPadding = PaddingValues(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(4.dp)
+                    ) {
+                        items(listOf("Valkeakoski", "Kerava", "GPS")) { city ->
+                            ListItem(
+                                modifier = Modifier.clickable {
+                                    text = city
+                                    active = false
+                                    when (city.lowercase()) {
+                                        "valkeakoski" -> viewModel.setCustomLocation(61.2628, 24.0316)
+                                        "kerava" -> viewModel.setCustomLocation(60.4039, 25.1015)
+                                        "gps" -> viewModel.useCurrentLocation()
+                                    }
+                                },
+                                headlineContent = {
+                                    Text(
+                                        text = city,
+                                        style = MaterialTheme.typography.titleMedium
+                                    )
+                                },
+                                supportingContent = {
+                                    Text("Select $city")
+                                },
+                                leadingContent = {
+                                    Icon(Icons.Rounded.LocationOn, contentDescription = null)
+                                },
+                            )
+                        }
+                    }
+                }
+
+                Button(
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .padding(top = 8.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                    onClick = { viewModel.toggleTemperatureUnit() },
+                    shape = RoundedCornerShape(12.dp),
+                    contentPadding = PaddingValues(12.dp),
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(12.dp))
+                            .gradientBackground()
+                            .size(width = 54.dp, height = 48.dp),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Text(
+                            text = if (isCelsius.value) "°F" else "°C",
+                            color = Color(0xFF262547),
+                            fontWeight = FontWeight.Bold,
+                            style = TextStyle(fontSize = 20.sp),
                         )
                     }
                 }
             }
-
-            Button(
-                modifier = Modifier.align(Alignment.CenterVertically),
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-                onClick = { viewModel.toggleTemperatureUnit() },
-                shape = RoundedCornerShape(12.dp),
-                contentPadding = PaddingValues(12.dp),
-
-            ) {
-                Box(
-                    modifier = Modifier
-                        .clip(RoundedCornerShape(12.dp))
-                        .gradientBackground()
-                        .size(width = 54.dp, height = 48.dp),
-                    contentAlignment = Alignment.Center,
-                ) {
-                    Text(
-                        text = if (isCelsius.value) "°F" else "°C",
-                        color = Color(0xFF262547),
-                        fontWeight = FontWeight.Bold,
-                        style = TextStyle(fontSize = 20.sp),
-                    )
-                }
-            }
         }
-
 
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
             if (isDataLoading) {
@@ -238,6 +222,3 @@ fun App() {
         }
     }
 }
-
-
-
